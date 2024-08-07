4ebb3dcde4dd40ce1b38eeff18e314bf26126bf1
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-07 11:51:19 +0200

    Implement auth user endpoint

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -6,7 +6,22 @@ import generateToken from '../utils/generateToken.js';
 //route POST /api/users/auth
 //@access Public
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Auth User' });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPasswords(password))) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 //@desc Register new user
@@ -22,13 +37,14 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('User already exists');
   }
 
-  const user = User.create({
+  const user = await User.create({
     name,
     email,
     password,
   });
 
   if (user) {
+    console.log(user);
     generateToken(res, user._id);
 
     res.status(201).json({
