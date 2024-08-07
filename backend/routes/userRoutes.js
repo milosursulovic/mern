5354bfcb10152dbab91cff9b4dd0ed87ad4ab09b
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-07 13:11:28 +0200

    Implement auth protect middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { protect } from '../middlewares/authMiddleware.js';
 import {
   authUser,
   registerUser,
@@ -6,11 +7,15 @@ import {
   getUserProfile,
   updateUserProfile,
 } from '../controllers/userControllers.js';
+
 const router = express.Router();
 
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotnev from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 
 dotnev.config();
 connectDB();
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => res.send('Server is ready'));
