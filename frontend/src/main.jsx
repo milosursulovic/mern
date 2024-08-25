7e4a7ba895c044db8a74c4ed69a9e94bd230da07
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-25 22:02:56 +0200

    Add react router setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import HomeScreen from './screens/HomeScreen.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>,
+  ),
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
