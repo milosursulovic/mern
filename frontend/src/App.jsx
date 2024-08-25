f14519779de90a9e72b1edf86b299673ea2bc492
Milos Ursulovic <milosursulovic2696@gmail.com>
2024-08-25 14:31:35 +0200

    Add home screen and hero

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
       <Header />
-      <h1>App</h1>
+      <HomeScreen />
     </>
   );
 };
